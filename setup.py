f56e5c7b642369505615442c4fdfe4d00cd069f3
Corentin Chary <c.chary@criteo.com>
2016-05-10 09:16:25 +0200

    bump to 0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(fname):
 
 setup(
     name="carbonate-utils",
-    version="0.1",
+    version="0.2",
     author="Brice Arnould",
     author_email="b.arnould@criteo.com",
     maintainer="Corentin Chary",
